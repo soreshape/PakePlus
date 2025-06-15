4075f0baa0518f53593881d2a74170b1948bc3ff
soreshape <90969443+soreshape@users.noreply.github.com>
2025-06-15 16:32:39 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,5 +1,37 @@
-console.log('custom.js ----')
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
+//function clearCookies(){var cookies=document.cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='}}function parseCookies(cookie){clearCookies();var cookies=cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='+vals.join('=')}}
+//parseCookies("pt_pin=jd_JWnDpRqcgnaf; pt_key=AAJoSqQNADBQTBrcfAAIKfljmp7GzTtAio_dBnJnt3KPniTNAuxUNHJj9piQ_jpNOsng2_JhpHI")
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
+document.cookie="pt_pin=jd_wpEmxQsOpbqV; domain=.jd.com; path=/;"
+document.cookie="pt_key=AAJoSqPMADDStv9bfZg1Gs2R_XI8MFh6eiuN_6pZ_RswBX_ATfVrEJ-JaISxmzSiOjngV3P50CU; domain=.jd.com; path=/;"
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
+const hookClick = (e) => {
+    const origin = e.target.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://ip-api.com/json");
+oReq.send();
